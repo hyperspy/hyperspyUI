59bd19d2469c03aea94408d2a9d68c0e7f8049ad
vidartf <vidartf@gmail.com>
2014-11-17 12:31:22 +0100

    Added setup.py and __init__.py

---
+++ hyperspyui/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 17 12:30:07 2014
+
+@author: vidarton
+"""
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 17 11:58:16 2014
+
+@author: vidarton
+"""
+
+from distutils.core import setup
+
+setup(name='hyperspyUI',
+      version='1.0a',
+      description='Hyperspy Graphical Interface',
+      author='Vidar Tonaas Fauske',
+      author_email='vidartf+hyperspyui@gmail.com',
+      packages=['hyperspyui'],
+      requires=['hyperspy', 
+                'matplotlib (>= 1.4)', 
+                'python_qt_binding'],
+     )
