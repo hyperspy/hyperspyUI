e87707a065f7f0387a40d36bba5678cf8c9aab94
Vidar Tonaas Fauske <vidartf@gmail.com>
2014-11-27 04:50:53 +0100

    Made registration batch file silent + ask for admin
    You cannot get a UAC prompt directly in batch, so it asks the user to
    run again with privileges.

--- Register Windows extensions.bat
+++ Register Windows extensions.bat
@@ -1,9 +1,13 @@
-ECHO OFF
+@ECHO OFF
 PUSHD %~dp0
 
-REG ADD "HKCR\HyperSpy.Document\DefaultIcon" /t REG_SZ /d %~dp0images\hyperspy.ico
+1>nul 2>nul REG ADD "HKCR\HyperSpy.Document\DefaultIcon" /t REG_SZ /f /d %~dp0images\hyperspy.ico
 
-ASSOC .hdf5=HyperSpy.Document
-FTYPE HyperSpy.Document="%%PYTHONPATH%%pythonw.exe" "%~dp0hyperspyui\main.py" "%%1" %%*
+1>nul 2>nul ASSOC .hdf5=HyperSpy.Document
+1>nul 2>nul FTYPE HyperSpy.Document="%%PYTHONPATH%%pythonw.exe" "%~dp0hyperspyui\main.py" "%%1" %%*
 
 POPD
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO You need to run this batch as an Administrator
+	PAUSE
+)
