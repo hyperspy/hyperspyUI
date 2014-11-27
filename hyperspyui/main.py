29dfccde0809e7abd600c2c6cec6095494d176e8
Vidar Tonaas Fauske <vidartf@gmail.com>
2014-11-27 07:40:33 +0100

    Added logfile for pythonw execution

--- hyperspyui/main.py
+++ hyperspyui/main.py
@@ -5,13 +5,16 @@ Created on Tue Nov 25 02:10:29 2014
 @author: Vidar Tonaas Fauske
 """
 
-import os
+import os, sys
         
 from python_qt_binding import QtGui, QtCore
 import info
 from singleapplication import get_app
 
 def main():
+    exe_dir, exe_name = os.path.split(sys.executable)
+    if exe_name.startswith('pythonw'):
+        sys.stdout = sys.stderr = open(os.path.dirname(__file__) + '/../hyperspyui.log', 'w')
     #TODO: Make single/multi a setting
     app = get_app('hyperspyui')     # Make sure we only have a single instance
     
