0177776bc037bc9ac27b2f1a85a3e4a910c8bda6
Vidar Tonaas Fauske <vidartf@gmail.com>
2014-11-27 04:19:31 +0100

    Added icon for files on windows

--- Register Windows extensions.bat
+++ Register Windows extensions.bat
@@ -1,10 +1,9 @@
 ECHO OFF
 PUSHD %~dp0
 
+REG ADD "HKCR\HyperSpy.Document\DefaultIcon" /t REG_SZ /d %~dp0images\hyperspy.ico
+
 ASSOC .hdf5=HyperSpy.Document
 FTYPE HyperSpy.Document="%%PYTHONPATH%%pythonw.exe" "%~dp0hyperspyui\main.py" "%%1" %%*
-
-REM HKEY_CLASSES_ROOT\MyTextFile\DefaultIcon
-REM     @="C:\WINDOWS\explorer.exe,2"
 
 POPD
