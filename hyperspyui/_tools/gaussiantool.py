52bf40dc5f6d9b0fb12e87cba20eb4ed349366c8
Vidar Tonaas Fauske <vidartf@gmail.com>
2014-12-07 14:54:30 +0100

    Improved GaussianTool
    Added right-click to remove gaussians, and small cleanups

--- hyperspyui/_tools/gaussiantool.py
+++ hyperspyui/_tools/gaussiantool.py
@@ -13,10 +13,11 @@ Created on Sun Dec 07 02:03:23 2014
 """
 
 import os
-import copy
+import numpy as np
+import collections
 from matplotlib.widgets import SpanSelector
 
-from hyperspy.components import Gaussian2
+from hyperspy.components import Gaussian, Gaussian2
 
 from figuretool import FigureTool
 from util import load_cursor
@@ -29,6 +30,7 @@ class GaussianTool(FigureTool):
         self.span = None
         self.cursor = load_cursor(os.path.dirname(__file__) + \
                                   '/../../images/picker.svg', 8, 8)
+        self._old_plot_comp = {}
         
     def get_name(self):
         return "Gaussian tool"
@@ -42,6 +44,64 @@ class GaussianTool(FigureTool):
     def is_selectable(self):
         return True
         
+    def _wire_wrapper(self, wrapper):
+        if wrapper is None:
+            return
+        m = wrapper.model
+        if m._plot.is_active():
+            self._old_plot_comp[wrapper] = m._plot_components
+            m.enable_plot_components()
+            for c in m:
+                if isinstance(c, (Gaussian, Gaussian2)):
+                    c._model_plot_line.line.set_picker(True)
+                    
+    def _unwire_wrapper(self, wrapper):
+        if wrapper is None:
+            return
+        m = wrapper.model
+        if wrapper in self._old_plot_comp:
+            for c in m:
+                if isinstance(c, (Gaussian, Gaussian2)):
+                    c._model_plot_line.line.set_picker(False)
+            if not self._old_plot_comp[wrapper]:
+                m.disable_plot_components()
+        
+    def on_pick(self, event):
+        if event.mouseevent.inaxes is None:
+            return
+        ax = event.mouseevent.inaxes
+        if event.mouseevent.button == 3:
+            window = ax.figure.canvas.parent()
+            mw = window.property('hyperspyUI.ModelWrapper')
+            if mw is not None:
+                print event.artist
+                for c in mw.model:
+                    line = c._model_plot_line.line
+                    if event.artist == line and \
+                            isinstance(c, (Gaussian, Gaussian2)):
+                        mw.remove_component(c)      
+                
+                
+    def connect(self, windows):
+        super(GaussianTool, self).connect(windows)
+        if windows is None:
+            return
+        if not isinstance(windows, collections.Iterable):
+            windows = (windows,)
+        for w in windows:
+            mw = w.property('hyperspyUI.ModelWrapper')
+            self._wire_wrapper(mw)
+                
+    def disconnect(self, windows):
+        super(GaussianTool, self).disconnect(windows)
+        if windows is None:
+            return
+        if not isinstance(windows, collections.Iterable):
+            windows = (windows,)
+        for w in windows:
+            mw = w.property('hyperspyUI.ModelWrapper')
+            self._unwire_wrapper(mw)
+        
     def on_mousedown(self, event):
         if event.inaxes is None:
             return
@@ -49,13 +109,14 @@ class GaussianTool(FigureTool):
         f = ax.figure
         x, y = event.xdata, event.ydata
         
-        if event.dblclick:
+        if event.button == 1 and event.dblclick:
             # Add Gaussian here!
             window = f.canvas.parent()
             mw = window.property('hyperspyUI.ModelWrapper')
             if mw is None:
                 sw = window.property('hyperspyUI.SignalWrapper')
                 mw = sw.make_model()
+                self._wire_wrapper(mw)
             m = mw.model
             i = m.axis.value2index(x)
             h = m.spectrum()[i] - m()[i]
@@ -65,6 +126,7 @@ class GaussianTool(FigureTool):
             g.height.free = False
             g.centre.free = False
             mw.add_component(g)
+            g._model_plot_line.line.set_picker(True)
             m.fit_component(g, signal_range=None, estimate_parameters=False)
             g.height.free = True
             g.centre.free = True
@@ -81,10 +143,12 @@ class GaussianTool(FigureTool):
         if mw is None:
             sw = window.property('hyperspyUI.SignalWrapper')
             mw = sw.make_model()
+            self._wire_wrapper(mw)
         m = mw.model
         
         g = Gaussian2()
         mw.add_component(g)
+        g._model_plot_line.line.set_picker(True)
         m.fit_component(g, signal_range=(x0, x1))
         i = m.axis.value2index(g.centre.value)
         g.active = False
@@ -110,7 +174,11 @@ class GaussianTool(FigureTool):
         ax = self.drag_data[3]
         if self.span is None:
             self.dragging = False
-            self.span = SpanSelector(ax, self.on_spanselect, 'horizontal')
+            invtrans = ax.transData.inverted()
+            minspan = 3*np.abs(invtrans.transform((1,0)) - 
+                             invtrans.transform((0, 0)))[0]
+            self.span = SpanSelector(ax, self.on_spanselect, 'horizontal',
+                                     minspan=minspan)
             event2 = self.drag_data[4]
             self.span.press(event2)
             self.span.onmove(event)
