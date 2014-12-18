7edb7f09c7b69a104e9b978048eff7e239bdf863
Vidar Tonaas Fauske <vidartf@gmail.com>
2014-12-19 00:05:47 +0100

    Cleaned up pca
    Separated functionality, allows for 1D-nav PCA (in a better way than
    previously), and now prompts for conversion/copy (should also be put
    into setting with "Do not ask again" option). PCA is now also threaded.

--- hyperspyui/plugins/pca.py
+++ hyperspyui/plugins/pca.py
@@ -7,8 +7,16 @@ Created on Fri Dec 12 23:44:01 2014
 
 
 import plugin
-from hyperspyui.util import win2sig, fig2win
-from signalwrapper import SignalWrapper
+
+from python_qt_binding import QtGui, QtCore
+from QtCore import *
+from QtGui import *
+
+from hyperspyui.util import win2sig, fig2win, Namespace
+from hyperspyui.threaded import ProgressThreaded
+
+def tr(text):
+    return QCoreApplication.translate("PCA", text)
 
 class PCA_Plugin(plugin.Plugin):
     def create_actions(self):
@@ -30,38 +38,79 @@ class PCA_Plugin(plugin.Plugin):
             action.setEnabled(False)
         else:
             action.setEnabled(True)
-
-    def pca(self, signal=None):
+            
+            
+    def _get_signal(self, signal):
         if signal is None:
             signal = self.ui.get_selected_signal()
         s = signal.signal
-        
-        try:
-            s.decomposition()
-            ax = s.plot_explained_variance_ratio()  # Make scree plot
-        # decomp.. warns if wrong type, but plot_expl.. raises exception
-        except AttributeError:
-            # TODO: Messagebox prompt in place change or copy
-            s = s.deepcopy()
+
+        if s.data.dtype.char not in ['e', 'f', 'd']:  # If not float        
+            mb = QMessageBox(QMessageBox.Information, tr("Convert or copy"), 
+                             tr("Signal data has the wrong data type (float " + 
+                             "needed). Would you like to convert the current" +
+                             " signal, or perform the decomposition on a " +
+                             "copy?"))
+            convert = mb.addButton(tr("Convert"), QMessageBox.AcceptRole)
+            copy = mb.addButton(tr("Copy"), QMessageBox.RejectRole)
+            mb.addButton(QMessageBox.Cancel)
+            mb.exec_()
+            btn = mb.clickedButton()
+            if btn not in (convert, copy):
+                return
+            elif btn == copy: 
+                new_s = s.deepcopy()
+                if s.data.ndim == 2:
+                    bk_s_navigate = self.nav_dim_backups[s]
+                    s.axes_manager._set_axis_attribute_values('navigate', 
+                                                              bk_s_navigate)
+                s = new_s
+                self.ui.add_signal_figure(s, signal.name + "[float]")
             s.change_dtype(float)
-            s.decomposition()
-            ax = s.plot_explained_variance_ratio()
+        return s, signal
             
-        # Clean up plot and present, allow user to select components by picker
-        ax.set_title("")
-        scree = ax.get_figure().canvas
-        scree.draw()
-        scree.setWindowTitle("Pick number of components")
-        def clicked(event):
-            components = round(event.xdata)
-            # Num comp. picked, perform PCA, wrap new signal and plot
-            sc = s.get_decomposition_model(components)
-            scw = SignalWrapper(sc, self.ui, signal.name + "[PCA]")
-            self.ui.signals.append(scw)
-            # Close scree plot
-            w = fig2win(scree.figure, self.ui.figures)
-            w.close()
-        scree.mpl_connect('button_press_event', clicked)
+    def _do_decomposition(self, s, force=False):
+        if s.data.ndim == 2:
+            bk_s_navigate = \
+                    s.axes_manager._get_axis_attribute_values('navigate')
+            s.axes_manager.set_signal_dimension(1)
+        
+        if force or s.learning_results.explained_variance_ratio is None:
+            s.decomposition()
+        
+        if s.data.ndim == 2:
+            s.axes_manager._set_axis_attribute_values('navigate', 
+                                                      bk_s_navigate)
+        return s
+
+    def pca(self, signal=None):
+        ns = Namespace()
+        ns.s, signal = self._get_signal(signal)
+        
+        def do_threaded():
+            ns.s = self._do_decomposition(ns.s)
+            
+        def on_complete():
+            ax = ns.s.plot_explained_variance_ratio()
+                
+            # Clean up plot and present, allow user to select components by picker
+            ax.set_title("")
+            scree = ax.get_figure().canvas
+            scree.draw()
+            scree.setWindowTitle("Pick number of components")
+            def clicked(event):
+                components = round(event.xdata)
+                # Num comp. picked, perform PCA, wrap new signal and plot
+                sc = ns.s.get_decomposition_model(components)
+                self.ui.add_signal_figure(sc, signal.name + "[PCA]")
+                # Close scree plot
+                w = fig2win(scree.figure, self.ui.figures)
+                w.close()
+            scree.mpl_connect('button_press_event', clicked)
+            
+        t = ProgressThreaded(self.ui, do_threaded, on_complete, 
+                             label="Performing PCA")
+        t.run()
         
                         
                         
