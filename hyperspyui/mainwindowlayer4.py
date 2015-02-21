afb2eee146e5d62a801878b092fc6bf0e7580a10
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-02-21 16:38:42 +0100

    Moving around window layers, 2nd partial

---
+++ hyperspyui/mainwindowlayer4.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 21 16:09:45 2015
+
+@author: Vidar Tonaas Fauske
+"""
+
+from mainwindowlayer3 import MainWindowLayer3, tr
+
+from python_qt_binding import QtGui, QtCore
+from QtCore import *
+from QtGui import *
+  
+
+class MainWindowLayer4(MainWindowLayer3):
+    pass
