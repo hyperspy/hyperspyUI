b8f363ec3493af11e42dc1ac403c922b2cb59d09
vidartf <vidartf@gmail.com>
2015-02-27 19:39:06 +0100

    Revert "Undo change in master"
    This reverts commit e48d9d805799e35ccccbb88f67ac3a9a0ea531e0.

--- hyperspyui/tools.py
+++ hyperspyui/tools.py
@@ -12,6 +12,5 @@ from _tools.pointertool import PointerTool
 from _tools.hometool import HomeTool
 from _tools.zoompan import ZoomPanTool
 from _tools.gaussiantool import GaussianTool
-from _tools.croptool import CropTool
 
-default_tools = [PointerTool, HomeTool, ZoomPanTool, CropTool, GaussianTool]
+default_tools = [PointerTool, HomeTool, ZoomPanTool, GaussianTool]
