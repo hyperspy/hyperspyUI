25ac999a75b0828adcae8ff6417e8416d5ca9055
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-04-04 19:05:47 +0200

    Rebin now handles non-multiples
    The rebin plugin now handles rebinning of signals whose dimensions are
    not multiples of the factors specified. It does this by dropping away
    the edges, but will warn the user of this when the user specifies the
    factor.

--- hyperspyui/plugins/rebin.py
+++ hyperspyui/plugins/rebin.py
@@ -29,38 +29,75 @@ class RebinDialog(ExToolWindow):
 
     def rebin(self):
         shape = []
+        mods = []
         s = self.signal
         for ax in s.signal.axes_manager._get_axes_in_natural_order():
             spin = self.spins[ax.name]
             factor = spin.value()
+            if ax.size % factor == 0:
+                mods.append(slice(None))
+            else:
+                mods.append(slice(None, - (ax.size % factor)))
             shape.append(ax.size / factor)
+        # Crop to multiple of factors
+        s.signal.data = s.signal.data[tuple(mods)]
 
-        try:
-            s.signal = s.signal.rebin(shape)
-        except ValueError:
-            mb = QMessageBox(QMessageBox.Critical, tr("Invalid rebin factor"),
-                             tr("Can only rebin by factors of the original " +
-                                "dimensions (e.g. 4 can be rebinned by 1, 2 or " +
-                                "4, but not 3)."),
-                             QMessageBox.Ok)
-            mb.exec_()
-            return
+        # Update shape, but prevent auto_replot
+        old = s.signal.auto_replot
+        s.signal.auto_replot = False
+        s.signal.get_dimensions_from_data()
+        s.signal.auto_replot = old
+
+        # Do actual rebin
+        s.signal = s.signal.rebin(shape)
         self.ui.setUpdatesEnabled(False)
         try:
             s.replot()
         finally:
             self.ui.setUpdatesEnabled(True)    # Always resume updates!
 
+    def validate(self):
+        style = QApplication.style()
+        tmpIcon = style.standardIcon(style.SP_MessageBoxWarning, None, self)
+        s = self.signal
+        for ax in s.signal.axes_manager._get_axes_in_natural_order():
+            spin = self.spins[ax.name]
+            hbox = self.hboxes[ax.name]
+            if ax.size % spin.value() != 0:
+                # Not a factor, show warning
+                if hbox.count() <= 1:
+                    # No warning icon yet
+                    iconSize = spin.height()
+                    pm = tmpIcon.pixmap(iconSize, iconSize)
+                    lbl = QLabel()
+                    lbl.setPixmap(pm)
+                    lbl.setToolTip(tr("Not a factor of shape. Input data " +
+                                      "will be trimmed before binning."))
+                    sp = lbl.sizePolicy()
+                    sp.setHorizontalPolicy(QSizePolicy.Maximum)
+                    lbl.setSizePolicy(sp)
+                    hbox.insertWidget(0, lbl)
+            else:
+                # Everything OK, remove warning icon if there
+                if hbox.count() > 1:
+                    lbl = hbox.takeAt(0)
+                    lbl.widget().deleteLater()
+
     def create_controls(self):
         self.spins = {}
+        self.hboxes = {}
         form = QFormLayout()
         for ax in self.signal.signal.axes_manager._get_axes_in_natural_order():
             spin = QSpinBox(self)
             spin.setValue(1)
             spin.setMinimum(1)
+            spin.valueChanged.connect(self.validate)
             self.spins[ax.name] = spin
-            form.addRow(ax.name, spin)
-
+            hbox = QHBoxLayout()
+            hbox.addWidget(spin)
+            form.addRow(ax.name, hbox)
+            self.hboxes[ax.name] = hbox
+        self.form = form
         self.btn_rebin = QPushButton(tr("Rebin"))
         self.btn_rebin.clicked.connect(self.rebin)
 
