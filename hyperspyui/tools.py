74a1627ddc38b7aef9e1f0352cdab1f658870163
vidartf <vidartf@gmail.com>
2015-04-17 19:07:37 +0200

    Compatibility with hyperspy/master

--- hyperspyui/tools.py
+++ hyperspyui/tools.py
@@ -8,7 +8,10 @@ Created on Sun Dec 07 03:06:55 2014
 
 from _tools.tool import Tool
 from _tools.figuretool import FigureTool
-from _tools.selectiontool import SelectionTool
+try:
+    from _tools.selectiontool import SelectionTool
+except ImportError:
+    pass
 from _tools.pointertool import PointerTool
 from _tools.hometool import HomeTool
 from _tools.zoompan import ZoomPanTool
