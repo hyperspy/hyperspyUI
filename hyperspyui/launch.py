74be6e1d8b9ce6fc006154f41a161f607d9abd31
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-04-17 03:56:49 +0200

    Updated TODOs

--- hyperspyui/launch.py
+++ hyperspyui/launch.py
@@ -20,7 +20,6 @@ from hyperspyui.singleapplication import get_app
 # TODO: Batch processing dialog (browse + drop&drop target)
 # TODO: Editor threading + parallell processing utils (w/batch input)
 # TODO: Layout save/restore (ignorable settings (_settings?))
-# TODO: Fix unloading of plugins' tools
 # TODO: Add xray element find tool
 # TODO: Add xray 3rd state: Element present, but not used for maps
 # TODO: Add xray background window tool for modifying those by auto
@@ -31,7 +30,6 @@ from hyperspyui.singleapplication import get_app
 # TODO: Utilities to combine signals into stack and reverse
 # TODO: Make editor tabs drag&dropable + default to all in one
 # TODO: Make data type changer handle RGBX data types.
-# TODO: SignalWrapper gets problems if we close the navigator plot
 
 
 # import logging
