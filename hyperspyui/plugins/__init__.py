7729faae44d6ae72aca817cc3bee3e18623e49c0
vidartf <vidartf@gmail.com>
2015-04-17 19:08:00 +0200

    User plugin handeling

--- hyperspyui/plugins/__init__.py
+++ hyperspyui/plugins/__init__.py
@@ -12,9 +12,10 @@ modules = glob.glob(os.path.dirname(__file__) + "/*.py")
 # TODO: In release form, we should support compiled plugins in pyc/pyo format
 __all__ = [os.path.splitext(os.path.basename(f))[0] for f in modules
            if not os.path.basename(f).startswith('_')]
-modules = glob.glob(os.path.dirname(__file__) + "/user_plugins/*.py")
-__all__.extend(['.'.join(('user_plugins',
-                         os.path.splitext(os.path.basename(f))[0]))
-               for f in modules
-               if not os.path.basename(f).startswith('_')])
+if os.path.exists(os.path.join(os.path.dirname(__file__), "user_plugins")):
+    modules = glob.glob(os.path.dirname(__file__) + "/user_plugins/*.py")
+    __all__.extend(['.'.join(('user_plugins',
+                             os.path.splitext(os.path.basename(f))[0]))
+                   for f in modules
+                   if not os.path.basename(f).startswith('_')])
 __all__ = list(set(__all__))    # Make unique as py/pyc/pyo all match
