ff4a979452e74c15f213118b461bf12e5a6837d5
vidartf <vidartf@gmail.com>
2015-07-22 15:57:12 +0200

    Plugin for tight_layout() button

---
+++ hyperspyui/plugins/tightlayout.py
@@ -0,0 +1,31 @@
+from hyperspyui.plugins.plugin import Plugin
+import numpy as np
+from hyperspy.hspy import *
+
+
+class Tightlayout(Plugin):
+    name = "TightLayout"
+
+    def create_actions(self):
+        self.add_action(
+            self.name + '.default', self.name, self.default,
+            icon="C:/github/hyperspyUI/hyperspyui/images/move.svg",
+            tip="")
+
+    def create_menu(self):
+        self.add_menuitem('Plot', self.ui.actions[self.name + '.default'])
+
+    def create_toolbars(self):
+        self.add_toolbar_button(
+            'Plot',
+            self.ui.actions[
+                self.name +
+                '.default'])
+
+    def default(self):
+        ui = self.ui
+        siglist = ui.hspy_signals
+        s = ui.get_selected_signal()
+        for p in (s._plot.signal_plot, s._plot.navigator_plot):
+            p.figure.tight_layout()
+            p.figure.canvas.draw()
