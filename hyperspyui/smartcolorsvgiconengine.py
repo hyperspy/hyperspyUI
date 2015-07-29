020ceb09b0083adf9cb51b634c04c72844ac6b85
vidartf <vidartf@gmail.com>
2015-07-29 22:42:44 +0200

    Tune of color replacements for smart SVG icons

--- hyperspyui/smartcolorsvgiconengine.py
+++ hyperspyui/smartcolorsvgiconengine.py
@@ -36,7 +36,10 @@ class SmartColorSVGIconEngine(QIconEngineV2):
         self._svgFiles = {}
         self._svgBuffers = {}
         self._color = QApplication.palette().foreground().color()
-        self._color_replacements = {'#000000': self._color.name()}
+        self._color_replacements = {
+            '#000000': self._color.name()
+            'black': self._color.name()
+            }
 
     def _make_cache_key(self, size, mode, state):
         return str(self) + str((size, mode, state))
