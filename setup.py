c80695e7956575112c5d1b78877d18d888c403cd
vidartf <vidartf@gmail.com>
2015-12-14 14:55:16 +0100

    Improved setup+readme

--- setup.py
+++ setup.py
@@ -11,46 +11,63 @@ from setuptools import setup, find_packages
 
 import hyperspyui.info
 
-setup(name='hyperspyUI',
-      version=hyperspyui.info.version,
-      description='Hyperspy Graphical Interface',
-      author='Vidar Tonaas Fauske',
-      author_email='vidartf+hyperspyui@gmail.com',
-      url='http://github.com/vidartf/hyperspyUI/',
-      license='GPLv3',
-      packages=find_packages(exclude=['tests*',
-                                      'hyperspyui.plugins.user_plugins']),
-      requires=['hyperspy (>= 0.8.1)',
-                'matplotlib (>= 1.3)',
-                'python_qt_binding',
-                'pyqode.python (>= 2.6.0)',
-                'autopep8',
-                'traits',
-                'traitsui'],
-      install_requires=['hyperspy >= 0.8.1',
-                        'matplotlib >= 1.3',
-                        'python_qt_binding',
-                        'pyqode.python >= 2.6.0',
-                        'autopep8',
-                        'traits',
-                        'traitsui'],
-      package_data={
-          'hyperspyui':
-          ['images/*.svg',
-           'images/*.png',
-           'images/*.ico',
-           'images/icon/*.png',
-           'images/icon/*.ico',
-           'images/attributions.txt',
-           'ipython_profile/*'],
-      },
-      entry_points={
-          'gui_scripts': [
-              'HyperSpyUI = hyperspyui.launch:main',
-           ]
-      },
-      scripts=[
-          'bin/hyperspyui_install.py',
-          # 'bin/hyperspyui_reg_linux_extensions.sh',
-      ],
-      )
+setup(
+    name='hyperspyUI',
+    version=hyperspyui.info.version,
+    description='Hyperspy Graphical User Interface',
+    author='Vidar Tonaas Fauske',
+    author_email='vidartf+hyperspyui@gmail.com',
+    url='http://github.com/vidartf/hyperspyUI/',
+    license='GPLv3',
+    packages=find_packages(exclude=['tests*',
+                                    'hyperspyui.plugins.user_plugins']),
+    requires=['hyperspy (>= 0.8.1)',
+              'matplotlib (>= 1.3)',
+              'python_qt_binding',
+              'pyqode.python (>= 2.6.0)',
+              'autopep8',
+              'traits',
+              'traitsui'],
+    install_requires=['hyperspy >= 0.8.1',
+                      'matplotlib >= 1.3',
+                      'python_qt_binding',
+                      'pyqode.python >= 2.6.0',
+                      'autopep8',
+                      'traits',
+                      'traitsui'],
+    package_data={
+        'hyperspyui':
+        ['images/*.svg',
+         'images/*.png',
+         'images/*.ico',
+         'images/icon/*.png',
+         'images/icon/*.ico',
+         'images/attributions.txt',
+         'ipython_profile/*'],
+    },
+    entry_points={
+        'gui_scripts': [
+            'HyperSpyUI = hyperspyui:main',
+         ]
+    },
+    scripts=[
+        'bin/hyperspyui_install.py',
+        # 'bin/hyperspyui_reg_linux_extensions.sh',
+    ],
+    keywords=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only",
+        "Environment :: MacOS X",
+        "Environment :: Win32 (MS Windows)",
+        "Environment :: X11 Applications :: Qt",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    )
