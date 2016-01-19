d200c3f556fcf72207c26559216e76c06bf27337
Vidar Tonaas Fauske <vidartf@gmail.com>
2016-01-19 23:27:38 +0100

    Added signal statistics action (menu entry only)

--- hyperspyui/plugins/basicsignal.py
+++ hyperspyui/plugins/basicsignal.py
@@ -5,6 +5,8 @@ Created on Sun Mar 01 15:20:55 2015
 @author: Vidar Tonaas Fauske
 """
 
+import sys
+
 from hyperspyui.plugins.plugin import Plugin
 
 from python_qt_binding import QtGui, QtCore
@@ -20,19 +22,43 @@ class BasicSignalPlugin(Plugin):
     name = "Basic signal tools"
 
     def create_actions(self):
-        self.add_action('mean', "Mean Signal",
+        self.add_action('stats', tr("Statistics"),
+                        self.statistics,
+                        icon=None,
+                        tip=tr("Print the signal statistics to the console."),
+                        selection_callback=self.ui.select_signal)
+
+        self.add_action('mean', tr("Mean Signal"),
                         self.mean,
                         icon=None,
-                        tip="Plot the mean of the current signal")
+                        tip=tr("Plot the mean of the current signal"),
+                        selection_callback=self.ui.select_signal)
 
-        self.add_action('sum', "Summed Signal",
+        self.add_action('sum', tr("Summed Signal"),
                         self.sum,
                         icon=None,
-                        tip="Plot the sum of the current signal")
+                        tip=tr("Plot the sum of the current signal"),
+                        selection_callback=self.ui.select_signal)
 
     def create_menu(self):
+        self.add_menuitem("Signal", self.ui.actions['stats'])
         self.add_menuitem("Signal", self.ui.actions['mean'])
         self.add_menuitem('Signal', self.ui.actions['sum'])
+
+    def statistics(self, signal=None):
+        if signal is None:
+            signal = self.ui.get_selected_signal()
+        # If we're using an external console for errors, we need to temporarily
+        # redirect stdout
+        _old_stdout = None
+        if self.ui.settings['Output to console'].lower() != 'true':
+            _old_stdout = sys.stdout
+            sys.stdout = self.ui.console.kernel.stdout
+        try:
+            signal.print_summary_statistics()
+        finally:
+            if self.ui.settings['Output to console'].lower() != 'true':
+                sys.stdout = _old_stdout
 
     def mean(self, signal=None):
         if signal is None:
@@ -40,6 +66,7 @@ class BasicSignalPlugin(Plugin):
         try:
             signal.mean().plot()
         except TypeError:
+            # hyperspy < 0.9 compatibility
             for ax in signal.axes_manager.navigation_axes:
                 signal = signal.mean(ax.index_in_array + 3j)
             signal.plot()
