333c759bcb646c85aeafa28059bd1e98e94b10cc
Vidar Tonaas Fauske <vidartf@gmail.com>
2016-01-22 18:13:46 +0100

    Debug util for tracing

--- hyperspyui/util.py
+++ hyperspyui/util.py
@@ -38,6 +38,14 @@ def lstrip(string, prefix):
             return string[len(prefix):]
 
 
+def debug_trace():
+    '''Set a tracepoint in the Python debugger that works with Qt'''
+    from PyQt4.QtCore import pyqtRemoveInputHook
+    from pdb import set_trace
+    pyqtRemoveInputHook()
+    set_trace()
+
+
 def fig2win(fig, windows):
     # Each figure has FigureCanvas as widget, canvas has figure property
     if fig is None:
