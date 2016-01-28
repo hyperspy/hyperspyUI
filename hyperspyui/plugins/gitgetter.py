0d85ba794123e52eb08325b5307dc15073689b40
vidartf <vidartf@gmail.com>
2016-01-28 21:36:41 +0100

    Decode branch list data to string

--- hyperspyui/plugins/gitgetter.py
+++ hyperspyui/plugins/gitgetter.py
@@ -56,7 +56,8 @@ def get_github_branches(repo_url):
     """
     import urllib.request, urllib.error, urllib.parse
     branch_url = repo_url + '/branches/all'
-    html = urllib.request.urlopen(branch_url).read()
+    res = urllib.request.urlopen(branch_url)
+    html = res.read().decode(resource.headers.get_content_charset())
     names = re.findall(
         '<a href=".*?" class="branch-name css-truncate-target">(.*?)</a>',
         html)
