e0b1e5cae3c755004dd9f4a2b0f85542cfe32871
Magnus Nord <magnunor@gmail.com>
2017-04-13 17:58:16 +0200

    Improve DPC FFT masking plugin

--- hyperspyui/plugins/dpcfftmask.py
+++ hyperspyui/plugins/dpcfftmask.py
@@ -6,12 +6,34 @@ from scipy.ndimage.filters import gaussian_filter
 
 
 class DpcFFTFilterShifts(Plugin):
-    name = "FFT filter beam shifts"
+    """
+    Do FFT filtering of x and y beam shift signals by removing the high
+    frequency contributions. This is useful for reducing the effects
+    from diffraction contrast, since these normally vary at a higher
+    frequency compared to the DPC contrast.
+
+    This is done by:
+    - Fourier transforming the signal
+    - Masking the low frequencies in the Fourier transformed signal
+    - Inverse Fourier transforming this masked signal
+    - Subtracting a factor of this masked and inverted signal from the
+        original signal
+    This process is done separately for each signal.
+
+    Input parameters:
+    Mask radius : number
+        The size of the mask used on the Fourier transformed signal.
+        A smaller number will subtract more of the signal.
+    Smoothing factor : number
+        The amount of intensity from the masked and inverted signal
+        which is subtracted from the original signal.
+    """
+    name = "DPC FFT filter beam shifts"
 
     def create_actions(self):
         self.add_action(
                 self.name + '.fft_filter_shifts',
-                self.name,
+                "FFT filter beam shift signal",
                 self.fft_filter_shifts,
                 tip="Do FFT filtering on a beam shift signal, to suppress "
                     " high frequencies ")
@@ -24,8 +46,10 @@ class DpcFFTFilterShifts(Plugin):
         ui = self.ui
         signals = [s.signal for s in ui.select_x_signals(
             2, ["dif02", "dif13"])]
-        dialog = StringInputDialog("Please enter a mask radius:", "20")
-        mask_radius = dialog.prompt_modal(rejection=0.0)
+        dialog = StringInputDialog("Mask radius:", "20")
+        mask_radius = dialog.prompt_modal(rejection=None)
+        if mask_radius is None:
+            return
         s_dif02 = signals[0]
         s_dif13 = signals[1]
         fft02 = np.fft.fftshift(np.fft.fft2(s_dif02.data))
@@ -47,14 +71,18 @@ class DpcFFTFilterShifts(Plugin):
         s_ifft02 = hs.signals.Signal2D(np.real(ifft02))
         s_ifft13 = hs.signals.Signal2D(np.real(ifft13))
         dialog = StringInputDialog("Smoothing factor:", "0.7")
-        smoothing_factor = dialog.prompt_modal(rejection=0.0)
+        smoothing_factor = dialog.prompt_modal(rejection=None)
+        if smoothing_factor is None:
+            return
         s_ifft02.data *= float(smoothing_factor)
         s_ifft13.data *= float(smoothing_factor)
         s_dif02_filtered = s_dif02 - s_ifft02
         s_dif13_filtered = s_dif13 - s_ifft13
         s_dif02_filtered.change_dtype('float32')
         s_dif13_filtered.change_dtype('float32')
-        s_dif02_filtered.metadata.General.title = 'dif02 filtered'
-        s_dif13_filtered.metadata.General.title = 'dif13 filtered'
+        name_02 = s_dif02_filtered.metadata.General.title + ' filtered'
+        name_13 = s_dif13_filtered.metadata.General.title + ' filtered'
+        s_dif02_filtered.metadata.General.title = name_02
+        s_dif13_filtered.metadata.General.title = name_13
         s_dif02_filtered.plot()
         s_dif13_filtered.plot()
