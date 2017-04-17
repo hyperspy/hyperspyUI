f324cfab73323c3f0969b64d3712349c044b5b00
Francisco de la Peña <frandelapena@gmail.com>
2017-04-17 11:46:18 +0100

    Add linux desktop integration

---
+++ hyperspyui/desktop_integration_linux.py
@@ -0,0 +1,70 @@
+import os
+from pathlib import Path
+import subprocess
+from hyperspy.io_plugins import io_plugins
+
+
+DESKTOP = \
+"""[Desktop Entry]
+Exec={} %F
+Name=HyperSpy UI
+Terminal=false
+MimeType={} # application/x-hdf;application/x-dm3;
+Icon={}/images/hyperspy.svg
+Type=Application
+X-MultipleArgs=true
+Categories=Science;Physics;DataVisualization;
+
+"""
+
+MIME = \
+"""<?xml version="1.0"?>
+<mime-info xmlns='http://www.freedesktop.org/standards/shared-mime-info'>
+<mime-type type="application/x-{}">
+<comment>{}</comment>
+{}
+</mime-type>
+</mime-info>
+"""
+
+_PKGDIR = Path(__file__).resolve().parent
+TYPES = ["image/tiff"]
+MIMEPATH = os.path.join(_PKGDIR, "mime")
+
+if not os.path.exists(MIMEPATH):
+    os.makedirs(MIMEPATH)
+
+for fformat in io_plugins:
+    name = fformat.format_name
+    if name in ["netCDF", "Signal2D", "Protoships", "TIFF"]:
+        continue
+    if name == "HDF5":
+        extensions = set(["hspy", "hdf5"])
+        defext = "hspy"
+    else:
+        extensions = set([ext.lower() for ext in fformat.file_extensions])
+        defext = fformat.file_extensions[fformat.default_extension]
+    extstr = "\n".join(
+        ["<glob pattern=\"*.{}\"/>".format(ext) for ext in extensions])
+    mime = MIME.format(defext, name, extstr)
+    TYPES.append("application/x-{}".format(defext))
+    fpath = os.path.join(MIMEPATH, "{}.xml".format(defext))
+    print("Writing {}".format(fpath))
+    with open(fpath, "w") as f:
+        f.write(mime)
+    # Register
+    try:
+        subprocess.run(['xdg-mime', 'install', fpath], check=True)
+    except:
+        pass
+if not os.environ.get('XDG_DATA_HOME'):
+    os.environ['XDG_DATA_HOME'] = os.path.expanduser('~/.local/share')
+p = subprocess.run(["which", "hyperspyui"], stdout=subprocess.PIPE)
+if p.stdout.decode('utf-8'):
+    EXEC = p.stdout.decode('utf-8').replace("\n", "")
+else:
+    EXEC = "hyperspyui"
+apps_dir = os.path.join(os.environ['XDG_DATA_HOME'], "applications/")
+with open(os.path.join(apps_dir, "hyperspyui.desktop"), "w") as f:
+    print("Writing hyperspyui.desktop to {}".format(apps_dir))
+    f.write(DESKTOP.format(EXEC, ";".join(TYPES), _PKGDIR))
