8fcb3de4d406cc471eccc67e56e433261853873d
Francisco de la Peña <frandelapena@gmail.com>
2017-04-18 19:01:39 +0100

    Make it modular

--- hyperspyui/desktop_integration_linux.py
+++ hyperspyui/desktop_integration_linux.py
@@ -5,7 +5,7 @@ from hyperspy.io_plugins import io_plugins
 
 
 DESKTOP = \
-"""[Desktop Entry]
+    """[Desktop Entry]
 Exec={} %F
 Name=HyperSpy UI
 Terminal=false
@@ -18,7 +18,7 @@ Categories=Science;Physics;DataVisualization;
 """
 
 MIME = \
-"""<?xml version="1.0"?>
+    """<?xml version="1.0"?>
 <mime-info xmlns='http://www.freedesktop.org/standards/shared-mime-info'>
 <mime-type type="application/x-{}">
 <comment>{}</comment>
@@ -28,26 +28,24 @@ MIME = \
 """
 
 _PKGDIR = Path(__file__).resolve().parent
-TYPES = ["image/tiff"]
 if not os.environ.get('XDG_DATA_HOME'):
     os.environ['XDG_DATA_HOME'] = os.path.expanduser('~/.local/share')
 MIMEPATH = os.path.join(os.environ["XDG_DATA_HOME"], "mime")
+APPS_DIR = os.path.join(os.environ['XDG_DATA_HOME'], "applications/")
 
-if not os.path.exists(MIMEPATH):
-    os.makedirs(MIMEPATH)
-if not os.path.exists(os.path.join(MIMEPATH, "packages")):
-    os.makedirs(os.path.join(MIMEPATH, "packages"))
 
-for fformat in io_plugins:
-    name = fformat.format_name
-    if name in ["netCDF", "Signal2D", "Protochips", "TIFF"]:
-        continue
+def create_mime_file(hspy_format, types=None, update_db=False):
+    if not os.path.exists(MIMEPATH):
+        os.makedirs(MIMEPATH)
+    if not os.path.exists(os.path.join(MIMEPATH, "packages")):
+        os.makedirs(os.path.join(MIMEPATH, "packages"))
+    name = hspy_format.format_name
     if name == "HDF5":
         extensions = set(["hspy", "hdf5"])
         defext = "hspy"
     else:
-        extensions = set([ext.lower() for ext in fformat.file_extensions])
-        defext = fformat.file_extensions[fformat.default_extension]
+        extensions = set([ext.lower() for ext in hspy_format.file_extensions])
+        defext = hspy_format.file_extensions[hspy_format.default_extension]
     extstr = "\n".join(
         ["<glob pattern=\"*.{}\"/>".format(ext) for ext in extensions])
     if name == "HDF5":
@@ -56,26 +54,44 @@ for fformat in io_plugins:
         extstr += '<sub-class-of type="text/plain"/>\n'
     mime = MIME.format(defext, name, extstr)
 
-    TYPES.append("application/x-{}".format(defext))
     fpath = os.path.join(
         MIMEPATH, "packages", "application-hyperspy-{}.xml".format(defext))
     print("Writing {}".format(fpath))
     with open(fpath, "w") as f:
         f.write(mime)
-    # Register
-print("Updating mime database")
-subprocess.run(['update-mime-database',  MIMEPATH])
-if not os.environ.get('XDG_DATA_HOME'):
-    os.environ['XDG_DATA_HOME'] = os.path.expanduser('~/.local/share')
-p = subprocess.run(["which", "hyperspyui"], stdout=subprocess.PIPE)
-if p.stdout.decode('utf-8'):
-    EXEC = p.stdout.decode('utf-8').replace("\n", "")
-else:
-    EXEC = "hyperspyui"
-apps_dir = os.path.join(os.environ['XDG_DATA_HOME'], "applications/")
-with open(os.path.join(apps_dir, "hyperspyui.desktop"), "w") as f:
-    print("Writing hyperspyui.desktop to {}".format(apps_dir))
-    f.write(DESKTOP.format(EXEC, ";".join(TYPES), _PKGDIR))
+    if types:
+        types.append("application/x-{}".format(defext))
+    if update_db:
+        print("Updating mime database")
+        subprocess.run(['update-mime-database',  MIMEPATH])
 
-print("Updating desktop database")
-subprocess.run(['update-desktop-database', apps_dir])
+
+def get_hyperspyui_exec_path():
+    p = subprocess.run(["which", "hyperspyui"], stdout=subprocess.PIPE)
+    if p.stdout.decode('utf-8'):
+        hspyui_exec_path = p.stdout.decode('utf-8').replace("\n", "")
+    else:
+        hspyui_exec_path = "hyperspyui"
+    return hspyui_exec_path
+
+
+def write_desktop_file(types=""):
+    with open(os.path.join(APPS_DIR, "hyperspyui.desktop"), "w") as f:
+        print("Writing hyperspyui.desktop to {}".format(APPS_DIR))
+        f.write(DESKTOP.format(get_hyperspyui_exec_path,
+                               ";".join(types), _PKGDIR))
+
+def run_desktop_integration(
+        exclude_formats=["netCDF", "Signal2D", "Protochips", "TIFF"]):
+    types = ["image/tiff"]
+    for hspy_format in io_plugins:
+        if hspy_format.format_name not in exclude_formats:
+            create_mime_file(hspy_format=hspy_format, types=types)
+    print("Updating mime database")
+    subprocess.run(['update-mime-database',  MIMEPATH])
+    write_desktop_file(types=types)
+    print("Updating desktop database")
+    subprocess.run(['update-desktop-database', APPS_DIR])
+
+if __name__ == "__main__":
+    run_desktop_integration()
