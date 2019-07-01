160d464c544c5b947f3f70e8e3297d14a3a3186a
Thomas Aarholt <thomasaarholt@gmail.com>
2019-07-01 19:20:09 +0200

    add support for EELS zlp also_align

--- hyperspyui/plugins/alignzlp.py
+++ hyperspyui/plugins/alignzlp.py
@@ -1,6 +1,7 @@
 from hyperspyui.plugins.plugin import Plugin
-
-
+from hyperspyui.widgets.signallist import SignalList
+from hyperspyui.log import logger
+from qtpy.QtWidgets import QDialog
 class Alignzlp(Plugin):
     name = "AlignZLP"
 
@@ -24,4 +25,22 @@ class Alignzlp(Plugin):
     def default(self):
         ui = self.ui
         s = ui.get_selected_signal()
-        s.align_zero_loss_peak()
+        logger.debug('Align zero loss peak of ' + str(s))
+        title = s.metadata.General.title
+
+        signal_list = [sig for sig in ui.signals if sig.signal is not s]
+        picker = SignalList(items=signal_list, parent=ui, multiselect=True)
+        diag = ui.show_okcancel_dialog("Select signals to align with {}".format(
+            title), picker, modal=True)
+        signals = []
+        if diag.result() == QDialog.Accepted:
+            signals = picker.get_selected()
+        else:
+            return
+        signals = [] if not signals else signals  # if none selected
+        signals = [sig.signal for sig in signals]
+        logger.debug('Also aligning the following signals\n' + str(signals))
+        # unclear how to incorporate this. It needs a blist argument
+        # picker.unbind(blist=?)
+        s.align_zero_loss_peak(also_align=signals)
+        logger.debug('ZLP alignment complete')
