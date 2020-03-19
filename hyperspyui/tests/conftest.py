f36d65efe4e0efffae2fcf6ebfc9e9fa17edc3d8
Eric Prestat <eric.prestat@gmail.com>
2020-03-19 18:03:18 +0000

    Fix issue with initialization of QtWebEngineWidgets when running the test suite.

--- hyperspyui/tests/conftest.py
+++ hyperspyui/tests/conftest.py
@@ -10,6 +10,11 @@ import pytest
 
 from qtpy import QtCore
 
+# QtWebEngineWidgets must be imported before a QCoreApplication instance 
+# is created (used in eelsdb plugin)
+# Avoid a bug in Qt: https://bugreports.qt.io/browse/QTBUG-46720
+from qtpy import QtWebEngineWidgets
+
 QCoreApplication = QtCore.QCoreApplication
 QSettings = QtCore.QSettings
 
