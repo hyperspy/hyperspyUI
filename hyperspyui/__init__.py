76d391f96a6bff65432e9c440d4f0b7879f8f6b1
Eric Prestat <eric.prestat@gmail.com>
2023-12-04 19:35:53 +0000

    Use `setuptools_scm` to manage version

--- hyperspyui/__init__.py
+++ hyperspyui/__init__.py
@@ -15,11 +15,31 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with HyperSpyUI.  If not, see <http://www.gnu.org/licenses/>.
-"""
-Created on Mon Nov 17 12:30:07 2014
 
-@author: Vidar Tonaas Fauske
-"""
+from importlib.metadata import version
+from pathlib import Path
 
 
-from hyperspyui.version import __version__
+__version__ = version("exspy")
+
+# For development version, `setuptools_scm` will be used at build time
+# to get the dev version, in case of missing vcs information (git archive,
+# shallow repository), the fallback version defined in pyproject.toml will
+# be used
+
+# If we have an editable installed from a git repository try to use
+# `setuptools_scm` to find a more accurate version:
+# `importlib.metadata` will provide the version at installation
+# time and for editable version this may be different
+
+# we only do that if we have enough git history, e.g. not shallow checkout
+_root = Path(__file__).resolve().parents[1]
+if (_root / ".git").exists() and not (_root / ".git/shallow").exists():
+    try:
+        # setuptools_scm may not be installed
+        from setuptools_scm import get_version
+
+        __version__ = get_version(_root)
+    except ImportError:  # pragma: no cover
+        # setuptools_scm not installed, we keep the existing __version__
+        pass
