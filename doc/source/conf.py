e13be97c338e122e438de993b572d0c7995479f8
Eric Prestat <eric.prestat@gmail.com>
2025-01-15 19:04:29 +0000

    Switch to `pydata_sphinx_theme`

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,12 +34,12 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.coverage",
     "sphinx.ext.mathjax",
-    "sphinx_toggleprompt",
+    "sphinx_copybutton",
     "sphinx.ext.napoleon",
     "IPython.sphinxext.ipython_console_highlighting",
     "sphinxcontrib.towncrier",
+    "sphinx_favicon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -119,7 +119,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
